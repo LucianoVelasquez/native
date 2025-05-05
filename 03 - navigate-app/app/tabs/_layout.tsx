07af195f109e79ea7629032276e31ecc9ae4e641
LucianoVelasquez <125805986+LucianoVelasquez@users.noreply.github.com>
2025-05-05 15:19:38 -0300

    feat: actualiza la navegación de la aplicación y añade nuevas pantallas
    Se modifica la redirección inicial para llevar a la pantalla de pestañas. Se crea un nuevo layout de pestañas que incluye las pantallas de "Home" y "Favorite", cada una con su respectivo contenido. Esto mejora la estructura de navegación y la experiencia del usuario en la aplicación.

---
+++ 03 - navigate-app/app/tabs/_layout.tsx
@@ -0,0 +1,27 @@
+import { Redirect, Tabs } from "expo-router";
+import { Text } from "react-native";
+import { Ionicons } from '@expo/vector-icons';
+
+
+export default function TabsLayout() {
+  return (
+    <Tabs screenOptions={{ tabBarActiveTintColor: 'blue' }}>
+      <Tabs.Screen
+        name="home/index"
+        options={{
+          title: 'Home',
+          tabBarIcon: ({ color }) => <Ionicons size={28} name="home-outline" color={color} />,
+          headerTitleAlign: 'center',
+        }}
+      />
+      <Tabs.Screen
+        name="favorite/index"
+        options={{
+          title: 'Favorite',
+          tabBarIcon: ({ color }) => <Ionicons size={28} name="heart-outline" color={color} />,
+          headerTitleAlign: 'center',
+        }}
+      />
+    </Tabs>
+  );
+}
---
+++ 03 - navigate-app/app/tabs/favorite/index.tsx
@@ -0,0 +1,11 @@
+
+import { View, Text } from "react-native";
+export default function FavoriteTabs() {
+
+    return (
+        <View className="flex-1 items-center justify-center">
+            <Text>Favorite</Text>
+        </View>
+    )
+
+}
